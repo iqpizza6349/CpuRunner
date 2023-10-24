2b68a80792d9754f5700a0a8af15a6adb3282075
iqpizza6349 <iqpizza6349@gmail.com>
2023-10-24 17:04:38 +0900

    Rename 'CharacterWidget' to 'RunnerWidget'

---
+++ src/include/CpuRunner/RunnerWidget.h
@@ -0,0 +1,46 @@
+#pragma once
+#include <CpuRunner/Widget.h>
+#include <CpuRunner/UsageLabel.h>
+
+#include <QPixmap>
+#include <QPoint>
+#include <QTimer>
+#include <QMouseEvent>
+
+#include <string>
+
+namespace CpuRunner {
+    class UsageLabel;
+
+    class RunnerWidget : public QWidget {
+    public:
+        RunnerWidget();
+        
+    protected:
+        void mousePressEvent(QMouseEvent *event) override {
+            offset = event->pos();
+        }
+
+        void mouseMoveEvent(QMouseEvent *event) override {
+            if (event->buttons() && Qt::LeftButton) {
+                QPoint newPos = event->globalPos() - offset;
+                move(newPos);
+            }
+        }
+
+    public slots:
+        void update();
+
+    private:
+        QLabel *label;
+        QPoint offset;
+        QTimer *guiTimer;
+        std::string previous;
+        UsageLabel *usageLabel;
+
+        void updateGUI(QPixmap pixmap);
+        bool checkIsSameAsPrevious(std::string newString) {
+            return previous == newString;
+        }
+    };
+}
