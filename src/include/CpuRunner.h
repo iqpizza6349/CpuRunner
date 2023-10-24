b8f0d4d69bd94bb21550661edfceba3ba0b7aae5
iqpizza6349 <iqpizza6349@gmail.com>
2023-10-24 17:07:27 +0900

    Make Root header file for CpuRunner

---
+++ src/include/CpuRunner.h
@@ -0,0 +1,18 @@
+#pragma once
+#ifndef __CPU_RUNNER_H__
+#define __CPU_RUNNER_H__
+
+#include <CpuRunner/RunnerWidget.h>
+#include <CpuRunner/UsageLabel.h>
+#include <CpuRunner/Widget.h>
+
+#include <QDebug>
+
+namespace CpuRunner {
+    //TODO: which is return the value from 'option.ini'
+    bool isDebug() {
+        return 1;
+    }
+}
+
+#endif
