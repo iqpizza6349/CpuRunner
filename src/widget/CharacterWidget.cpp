0c04eec672792955b4d2396293bb4edb54355754
iqpizza6349 <iqpizza6349@gmail.com>
2023-10-24 09:00:28 +0900

    Update image change by private-method

--- src/widget/CharacterWidget.cpp
+++ src/widget/CharacterWidget.cpp
@@ -1,17 +1,44 @@
 #include "CharacterWidget.h"
+#include <iostream>
+
+#include <CpuUsage.h>
 
 CharacterWidget::CharacterWidget(): QWidget() {
     label = new QLabel(this);
-    QPixmap pixmap("../assets/normal.png");
+
+    QPixmap pixmap(NORMAL);
+    updateImage(pixmap);
+
+    guiTimer = new QTimer(this);
+    connect(guiTimer, &QTimer::timeout, this, &CharacterWidget::updateCpuUsage);
+    guiTimer->start(1000);
+}
+
+CharacterWidget::~CharacterWidget() {
+}
+
+bool CharacterWidget::checkIsSameAsPrevious(std::string newString) {
+    return (previous == newString);
+}
+
+void CharacterWidget::updateCpuUsage() {
+    double usage = CpuUsage::usage();
+    std::string filename = (usage > 33.0) ? NORMAL : COOL;
+    if (!checkIsSameAsPrevious(filename)) {
+        previous = filename;
+        QString filepath = QString::fromStdString(filename);
+        QPixmap pixmap(filepath);
+        updateImage(pixmap);
+    }
+}
+
+void CharacterWidget::updateImage(QPixmap pixmap) {
     if (pixmap.isNull()) {
         qDebug() << "Failed to load image!";
     } else {
         this->setAutoFillBackground(true);
         this->setMask(pixmap.mask());
         label->setPixmap(pixmap);
+        label->adjustSize();
     }
-}
-
-CharacterWidget::~CharacterWidget() {
-    // destructor
 }
