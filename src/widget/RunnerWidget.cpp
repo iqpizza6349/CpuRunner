fdda87b9711c440904564d86e439b59946d4cec7
iqpizza6349 <iqpizza6349@gmail.com>
2023-10-24 17:08:46 +0900

    Create widget for entire application widget

---
+++ src/widget/RunnerWidget.cpp
@@ -0,0 +1,46 @@
+#include <CpuRunner/RunnerWidget.h>
+#include <iostream>
+
+#include <QImage>
+#include <QBitmap>
+#include <QString>
+#include <QDebug>
+
+#include <CpuUsage.h>
+
+namespace CpuRunner {
+    RunnerWidget::RunnerWidget(): QWidget() {
+        this->setAutoFillBackground(true);
+        label = new QLabel(this);
+        usageLabel = new UsageLabel(this);
+        
+        QPixmap pixmap(NORMAL);
+        updateGUI(pixmap);
+        
+        guiTimer = new QTimer(this);
+        connect(guiTimer, &QTimer::timeout, this, &RunnerWidget::update);
+        guiTimer->start(1000);
+    }
+    
+    void RunnerWidget::update() {
+        double usage = CpuUsage::usage();
+        usageLabel->updateUsage(static_cast<int>(usage));
+        std::string filename = (usage > 33.0) ? NORMAL : COOL;
+        if (!checkIsSameAsPrevious(filename)) {
+            previous = filename;
+            QString filepath = QString::fromStdString(filename);
+            QPixmap pixmap(filepath);
+            updateGUI(pixmap);
+        }
+    }
+
+    void RunnerWidget::updateGUI(QPixmap pixmap) {
+        if (pixmap.isNull()) {
+            qDebug() << "Failed to load image!";
+        } else {
+            this->setMask(pixmap.mask());
+            label->setPixmap(pixmap);
+            label->adjustSize();
+        }
+    }
+}
