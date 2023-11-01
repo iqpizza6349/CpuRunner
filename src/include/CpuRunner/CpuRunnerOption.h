c0d42d79447c1cb848a95db47dbe235043eb494f
iqpizza6349 <iqpizza6349@gmail.com>
2023-11-01 09:13:08 +0900

    Define cpu runner application's options structure

---
+++ src/include/CpuRunner/CpuRunnerOption.h
@@ -0,0 +1,25 @@
+
+#include <string>
+#include <unordered_map>
+
+namespace CpuRunner {
+    struct CpuRunnerOption {
+        enum class Option {
+            DEBUG_DEBUG,
+            DEBUG_LOGGING,
+            WIDGET_IMAGES,
+            WIDGET_WIDTH,
+            WIDGET_HEIGHT,
+            WIDGET_TOP,
+            WIDGET_SLEEP
+        };
+
+        /// @brief Implements in OptionsManager.cpp
+        static const std::unordered_map<Option, std::string> enumToString;
+
+        static std::string toString(Option option) {
+            auto it = enumToString.find(option);
+            return (it != enumToString.end()) ? it->second : "Unknown";
+        }
+    };
+}
