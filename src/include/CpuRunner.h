2cfe73758f7ba7bf0f5425b875e8eaa0ef578fee
iqpizza6349 <iqpizza6349@gmail.com>
2023-11-01 15:02:25 +0900

    Remove unused headers and Add initialize function

--- src/include/CpuRunner.h
+++ src/include/CpuRunner.h
@@ -2,26 +2,16 @@
 #ifndef __CPU_RUNNER_H__
 #define __CPU_RUNNER_H__
 
-#include <CpuRunner/RunnerWidget.h>
-#include <CpuRunner/UsageLabel.h>
-#include <CpuRunner/Widget.h>
 #include <CpuRunner/OptionsManager.h>
 
 #include <QDebug>
 
 namespace CpuRunner {
-    const INI::Loader OptionsManager::nullLoader;
+    static OptionsManager *manager;
 
-    bool isDebug() {
-        OptionsManager *manager = OptionsManager::GetInstance();
-        std::variant<std::string, int> value = manager->getOptionValue(CpuRunnerOption::Option::DEBUG_DEBUG);
-        std::string str = manager->convertToString(value);
-        std::transform(str.begin(), str.end(), str.begin(), ::tolower);
-        std::istringstream is(str);
-        bool b;
-        is >> std::boolalpha >> b;
-        return b;
-    }
+    bool isDebug();
+
+    void initialize();
 }
 
 #endif
