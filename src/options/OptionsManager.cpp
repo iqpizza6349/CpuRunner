8fda0b6949da754e042bd0657e6da54771f4e4a3
iqpizza6349 <iqpizza6349@gmail.com>
2023-11-01 09:14:30 +0900

    Implements  enumeration

--- src/options/OptionsManager.cpp
+++ src/options/OptionsManager.cpp
@@ -1,6 +1,18 @@
 #include <CpuRunner/OptionsManager.h>
 
 namespace CpuRunner {
+
+    const std::unordered_map<CpuRunnerOption::Option, std::string> 
+            CpuRunnerOption::enumToString = {
+        { Option::DEBUG_DEBUG, "debug.debug" },
+        { Option::DEBUG_LOGGING, "debug.logging" },
+        { Option::WIDGET_IMAGES, "widget.images" },
+        { Option::WIDGET_WIDTH, "widget.widget_width" },
+        { Option::WIDGET_HEIGHT, "widget.widget_height" },
+        { Option::WIDGET_TOP, "widget.keep_top" },
+        { Option::WIDGET_SLEEP, "widget.could_sleep" }
+    };
+
     OptionsManager *OptionsManager::manager_ = nullptr;
 
     OptionsManager *OptionsManager::GetInstance(const INI::Loader &loader) {
